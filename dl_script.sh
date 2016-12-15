b722b5f5201741ce2287998e74a448d248ca2cf5
Pedrum Alt <pedrum.jalali3@gmail.com>
2016-12-15 11:55:31 -0500

    d

---
+++ dl_script.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#Reads lines form the config.txt file and downlaods the datasets. Unzips them and runs the program on them
+
+
+#Creates input and output files
+rm -rf dumpInput  
+mkdir dumpInput
+rm -rf Output  
+mkdir Output
+ 
+#reads config file for URL of datasets to download. 
+
+cat config.txt |
+while read x; do
+	print $x;
+	(cd dumpInput; wget $x);
+done
+
+#it is assumed the datasets have tar.gz extensions. If they have any other extension
+#this code must be updated
+(cd dumpInput; ls -l |	awk '{print $9}' |
+	while read x; do 
+		I=$((I+1));
+		if [ $I != 1 ]; then
+			gunzip ${x};
+		fi
+	done
+)
+
+gcc feat_extract.c
+
+#runs the script for each input file and produces an output file
+I=0
+(cd dumpInput; ls -l) |	awk '{print $9}' |
+	while read x; do 
+		I=$((I+1));
+		if [ $I != 1 ]; then
+			./a.out dumpInput/$x >> Output/output$((I-1)).csv ;
+		fi
+	done
+
+
--- script.sh
+++ script.sh
@@ -1,34 +1,17 @@
 #!/bin/bash
-STRING="Hello World"
+#reads files from dumpinput and outputs them in Ouput folder
 
 
-#Creates input and output files
-rm -rf dumpInput 
-mkdir dumpInput
-rm -rf Output
+#Createsoutput files
+rm -rf Output  
 mkdir Output
  
-#reads config file for URL of datasets to download. 
-cat config |
-while read x; do
-	print $x;
-	(cd dumpInput; wget $x);
-done
 
-#it is assumed the datasets have tar.gz extensions. If they have any other extension
-#this code must be updated
-(cd dumpInput; ls -l |	awk '{print $9}' |
-	while read x; do 
-		I=$((I+1));
-		if [ $I != 1 ]; then
-			gunzip ${x};
-		fi
-	done
-)
+
+gcc feat_extract.c
 
 #runs the script for each input file and produces an output file
 I=0
-rm Output/* >> /dev/null
 (cd dumpInput; ls -l) |	awk '{print $9}' |
 	while read x; do 
 		I=$((I+1));
